066621f56de74d7c2267e8497066979013b73b71
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2021-04-21 16:59:36 +0200

    initial cleaning of the project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
+import Root from "./Root";
+import "./index.css";
+import { ThemeProvider } from "theme-ui";
+import { mainTheme } from "./globalStyles/themes/mainTheme";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ThemeProvider theme={mainTheme}>
+    <RecoilRoot>
+      <Root />
+    </RecoilRoot>
+  </ThemeProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
